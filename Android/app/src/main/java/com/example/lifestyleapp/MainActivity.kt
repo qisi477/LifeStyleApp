0aa62ffa61ace11f7a62d20b9fa017d37323dede
QiSi <qisi477@gmail.com>
2020-09-09 11:07:05 -0600

    add register page

--- Android/app/src/main/java/com/example/lifestyleapp/MainActivity.kt
+++ Android/app/src/main/java/com/example/lifestyleapp/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.lifestyleapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lifestyleapp.activities.RegisterActivity
 import com.example.lifestyleapp.activities.SummaryActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, SummaryActivity::class.java)
+        val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
     }
 }
