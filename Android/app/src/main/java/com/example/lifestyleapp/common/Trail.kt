6ad6b767c33ddaada2c8a4d2e79a6debf4356644
Chad Hurst <chadhurst4@gmail.com>
2020-09-19 19:13:59 -0600

    added @Json(name = "") for all Trail fields

--- Android/app/src/main/java/com/example/lifestyleapp/common/Trail.kt
+++ Android/app/src/main/java/com/example/lifestyleapp/common/Trail.kt
@@ -1,27 +1,52 @@
 package com.example.lifestyleapp.common
 
+import com.beust.klaxon.Json
+
 data class Trail(
+    @Json(name = "id")
     val id: Int?,
+    @Json(name = "name")
     val name: String?,
+    @Json(name = "type")
     val type: String?,
+    @Json(name = "summary")
     val summary: String?,
+    @Json(name = "difficulty")
     val difficulty: String?,
+    @Json(name = "stars")
     val stars: Float?,
+    @Json(name = "starVotes")
     val starVotes: Int?,
+    @Json(name = "location")
     val location: String?,
+    @Json(name = "url")
     val url: String?,
+    @Json(name = "imgSqSmall")
     val imgSqSmall: String?,
+    @Json(name = "imgSmall")
     val imgSmall: String?,
+    @Json(name = "imgSmallMed")
     val imgSmallMed: String?,
+    @Json(name = "imgMedium")
     val imgMedium: String?,
+    @Json(name = "length")
     val length: Float?,
+    @Json(name = "ascent")
     val ascent: Int?,
+    @Json(name = "descent")
     val descent: Int?,
+    @Json(name = "high")
     val high: Int?,
+    @Json(name = "low")
     val low: Int?,
+    @Json(name = "longitude")
     val longitude: Float?,
+    @Json(name = "longitude")
     val latitude: Float?,
+    @Json(name = "conditionStatus")
     val conditionStatus: String?,
+    @Json(name = "conditionDetails")
     val conditionDetails: String?,
+    @Json(name = "conditionDate")
     val conditionDate: String?,
 )
